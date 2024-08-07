928eee8e0b76535e0ccf731f2bfc7352211c8cd7
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-08-07 02:39:55 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
-mkdir -p $HOME/.tmp_francinette
+# Define the temporary directory and the installation directory
+TMP_DIR="$HOME/sgoinfre/.tmp_francinette"
+INSTALL_DIR="$HOME/sgoinfre/francinette-image"
 
-git clone https://github.com/WaRtr0/francinette-image.git $HOME/.tmp_francinette/francinette-image
+# Create the temporary directory
+mkdir -p $TMP_DIR
 
-source $HOME/.tmp_francinette/francinette-image/utils/remove_docker.sh
-source $HOME/.tmp_francinette/francinette-image/utils/remove_zshrc.sh
+# Clone the repository into the temporary directory
+git clone https://github.com/mgrl39/francinette-sgoinfre.git $TMP_DIR/francinette-image
 
-rm -rf $HOME/francinette-image
-rm -rf $HOME/.tmp_francinette
+# Source the removal scripts
+source $TMP_DIR/francinette-image/utils/remove_docker.sh
+source $TMP_DIR/francinette-image/utils/remove_zshrc.sh
 
+# Remove the existing francinette-image directory and the temporary directory
+rm -rf $INSTALL_DIR
+rm -rf $TMP_DIR
+
+# Define color codes for terminal output
 WHITE='\033[0;37m' 
 BLUE='\033[0;36m'
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
+
+# Print success message
 echo -e "${BLUE}[Francinette] ${WHITE}Uninstalled ${GREEN}OK"
+
+# Restart the shell to apply changes
 exec "$SHELL"
