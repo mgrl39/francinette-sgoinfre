92ea2d290822fedaebb1a51454a222797dd18d70
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-08-07 13:37:49 +0200

    Update install_zshrc.sh
    Fix: sgoinfre path add

--- utils/install_zshrc.sh
+++ utils/install_zshrc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 RC_FILE="$HOME/.zshrc"
+SGOINFRE_DIR="$HOME/sgoinfre/francinette-image"
 
 # Check if Francinette configurations are already in .zshrc
 if ! grep "francinette-image" "$RC_FILE" &> /dev/null; then
@@ -14,13 +15,13 @@ if ! grep "francinette-image" "$RC_FILE" &> /dev/null; then
     # Add Docker image check and load
     printf "\nif ! docker image ls | grep 'francinette-image' &> /dev/null; then\n" >> "$RC_FILE"
     printf "\t\techo \"[Francinette] Loading the docker container\"\n" >> "$RC_FILE"
-    printf "\t\tdocker load < %s/francinette-image/francinette.tar\n" "$HOME" >> "$RC_FILE"
+    printf "\t\tdocker load < $SGOINFRE_DIR/francinette.tar\n" >> "$RC_FILE"
     printf "\t\texec \"$SHELL\"\n" >> "$RC_FILE"
     printf "fi\n" >> "$RC_FILE"
 
     # Add Docker container check and start
     printf "\nif ! docker ps | grep 'francinette-image' &> /dev/null; then\n" >> "$RC_FILE"
-    printf "\tif docker run -d -i -v /home:/home -v /goinfre:/goinfre -v /sgoinfre:/sgoinfre -v %s/francinette-image/logs:/francinette/logs-t --name run-paco francinette-image /bin/bash 2>&1 | grep 'already' &> /dev/null; then\n" "$HOME" >> "$RC_FILE"
+    printf "\tif docker run -d -i -v /home:/home -v /goinfre:/goinfre -v /sgoinfre:/sgoinfre -v $SGOINFRE_DIR/logs:/francinette/logs-t --name run-paco francinette-image /bin/bash 2>&1 | grep 'already' &> /dev/null; then\n" >> "$RC_FILE"
     printf "\t\tdocker start run-paco\n" >> "$RC_FILE"
     printf "\tfi\n" >> "$RC_FILE"
     printf "fi\n" >> "$RC_FILE"
@@ -28,11 +29,11 @@ fi
 
 # Add aliases if they don't exist
 if ! grep "francinette=" "$RC_FILE" &> /dev/null; then
-    printf "\nalias francinette=%s/francinette-image/run.sh\n" "$HOME" >> "$RC_FILE"
+    printf "\nalias francinette=$SGOINFRE_DIR/run.sh\n" >> "$RC_FILE"
 fi
 
 if ! grep "paco=" "$RC_FILE" &> /dev/null; then
-    printf "\nalias paco=%s/francinette-image/run.sh\n" "$HOME" >> "$RC_FILE"
+    printf "\nalias paco=$SGOINFRE_DIR/run.sh\n" >> "$RC_FILE"
 fi
 
 # Print completion message
