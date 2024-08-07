b1e22ec6e9c6d09f66ead14777f0f5ad57871928
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-08-07 02:53:25 +0200

    Update remove_zshrc.sh

--- utils/remove_zshrc.sh
+++ utils/remove_zshrc.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
-sed -i '/\b\(systemctl status docker\|grep francinette-image\|grep \"francinette-image\"\)\b/{N;N;N;N;d;}' $HOME/.zshrc
-sed -i '/\b\("francinette-image"\)\b/{N;N;N;N;d;}' $HOME/.zshrc
-sed -i '/\b\("francinette-image"\|docker ps\)\b/{N;N;N;N;d;}' $HOME/.zshrc
-sed -i "/\b\(alias paco\|alias francinette\)\b/d" $HOME/.zshrc
 
+# Define color codes for terminal output
 WHITE='\033[0;37m' 
 BLUE='\033[0;36m'
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 BWhite='\033[1;37m' 
-NC='\033[0m'
-echo -e "${BLUE}[Francinette] ${WHITE} zshrc params removed"
+NC='\033[0m' # No Color
+
+# Remove lines related to "systemctl status docker", "francinette-image" and related commands
+sed -i '/systemctl status docker/,+4d' $HOME/.zshrc
+sed -i '/grep francinette-image/,+4d' $HOME/.zshrc
+sed -i '/grep "francinette-image"/,+4d' $HOME/.zshrc
+
+# Remove lines containing "francinette-image" alone
+sed -i '/"francinette-image"/d' $HOME/.zshrc
+
+# Remove lines containing docker ps or "francinette-image"
+sed -i '/docker ps/,+4d' $HOME/.zshrc
+sed -i '/"francinette-image"/,+4d' $HOME/.zshrc
+
+# Remove alias definitions for paco and francinette
+sed -i '/alias paco=/d' $HOME/.zshrc
+sed -i '/alias francinette=/d' $HOME/.zshrc
+
+# Print completion message
+echo -e "${BLUE}[Francinette] ${WHITE}zshrc params removed ${GREEN}successfully!"
