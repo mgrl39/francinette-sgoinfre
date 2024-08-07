9ef13819f1c9016d41dfbbd6eb16873b2fbec985
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-08-07 02:51:21 +0200

    Update move_tmp.sh

--- utils/move_tmp.sh
+++ utils/move_tmp.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-mkdir -p $HOME/.tmp_francinette
-if ls -l $HOME/francinette-image | grep "francinette.tar" &> /dev/null; then
-    mv /home/$USER/francinette-image/francinette.tar /home/$USER/.tmp_francinette/
+
+# Define the directories
+TMP_DIR="$HOME/sgoinfre/.tmp_francinette"
+FRANCINETE_DIR="$HOME/sgoinfre/francinette-image"
+FRANCINETE_TAR="$FRANCINETE_DIR/francinette.tar"
+
+# Create the temporary directory if it doesn't exist
+mkdir -p "$TMP_DIR"
+
+# Check if the francinette.tar file exists and move it to the temporary directory
+if [ -f "$FRANCINETE_TAR" ]; then
+    mv "$FRANCINETE_TAR" "$TMP_DIR/"
+    echo "Moved francinette.tar to $TMP_DIR"
+else
+    echo "francinette.tar not found in $FRANCINETE_DIR"
 fi
